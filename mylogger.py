3b2bf2844b951d7d252b55bfd455205a7e0006d6
Nicolo Valle <nicolo.valle@cern.ch>
2024-08-14 22:14:22 +0200

    python logger feature

--- mylogger.py
+++ mylogger.py
@@ -11,6 +11,10 @@ DEBUG = 'DEBUG'
 class Logger:
     def __init__(self, logfile):
         self.logfile = logfile
+        self.highligh_keyword = False
+
+    def set_highlight_keyword(self, hl: bool):
+        self.highlight_keyword = hl
 
     def cropstr(self, s, n):
         n = max(n, 4)
@@ -25,8 +29,22 @@ class Logger:
         tt = datetime.now()
         tstamp = "%s-%s-%s-%s:%s:%s" % (str(tt.year)[-2:], str(tt.month).zfill(2), str(tt.day).zfill(2), str(tt.hour).zfill(2), str(tt.minute).zfill(2), str(tt.second).zfill(2))
         coldict = {INFO: '92', WARNING: '38;5;214', ERROR: '91', FATAL: '97;41', DEBUG: '0'}
+
+        formatted_message_parts = []
+        for part in message:
+            words = str(part).split()
+            formatted_words = []
+            for word in words:
+                if self.highlight_keyword and word in [INFO,WARNING,ERROR,FATAL,DEBUG]:
+                    formatted_words.append("\033[%sm%s\033[0m" % (coldict[word], word))
+                else:
+                    formatted_words.append(word)
+
+            formatted_message_parts.append(' '.join(formatted_words))
+        formatted_message = ' '.join(formatted_message_parts)
+                
         
-        print("\033[%sm[%s][%s][%s]\033[0m" % (coldict[severity], tstamp, severity, filename + ':' + funcname), *message)
+        print("\033[%sm[%s][%s][%s]\033[0m %s" % (coldict[severity], tstamp, severity, filename + ':' + funcname, formatted_message))
 
         writestring = ' '.join(map(str, message))
         with open(self.logfile, 'a') as f:
